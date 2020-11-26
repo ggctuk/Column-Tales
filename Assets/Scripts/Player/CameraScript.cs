13d32f31aa5f081dd6ed3c1a992b29848bd99ded
giftheck <1804007@students.ucreative.ac.uk>
2020-11-26 10:46:57 +0000

    V0.0001
    This is a test full of stuff

---
+++ Assets/Scripts/Player/CameraScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public Transform cameraTarget = null;
+    public Vector3 followDamp = new Vector3(25.0F, 25.0F, 0.0F);
+
+    private float currentVelocityX = 0;
+    private float currentVelocityY = 0;
+    private float currentVelocityZ = 0;
+
+    private void LateUpdate()
+    {
+        Vector3 position = transform.position;
+        position.x = Mathf.SmoothDamp(position.x, cameraTarget.transform.position.x, ref currentVelocityX, followDamp.x * Time.deltaTime);
+        position.y = Mathf.SmoothDamp(position.y, cameraTarget.transform.position.y, ref currentVelocityX, followDamp.y * Time.deltaTime);
+        position.z = Mathf.SmoothDamp(position.z, cameraTarget.transform.position.z, ref currentVelocityX, followDamp.z * Time.deltaTime);
+    }
+}
--- Assets/Scripts/Player/PlayerControllerScript.cs
+++ Assets/Scripts/Player/PlayerControllerScript.cs
@@ -4,15 +4,86 @@ using UnityEngine;
 
 public class PlayerControllerScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //Define the character controller first and foremost
+    public Camera playerCamera;
+    public CharacterController characterController;
+
+    //Define the audio stuff.
+    public AudioSource playerAudioSource;
+    public AudioClip playerJumpingSound;
+    public AudioClip playerAttackSound;
+    public AudioClip playerHurtSound;
+    public AudioClip playerDeathSound;
+
+    //Time to define some other player controller stuff
+    Transform model;
+
+    //Define values for things like speed, jump and character gravity
+    public float speed = 6.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+    public float maxJumps = 1; //The maximum n umber of times the character can jump. 1 is from ground. Any more is from the air.
+
+    private Vector3 moveDirection = Vector3.zero;
+    private float currentSpeed = 0.0f; //Default movement speed. 0 means character is standing when spawned
+    private float currentJumps = 0; //Default number of current number of jumps executed. 0 means character hasn't jumped by default
+    
+    //Okay, so let's now define the character controller stuff at the start of spawning
     void Start()
     {
-        
+        characterController = GetComponent<CharacterController>();
+        model = transform.Find("Model"); //The actual player model must be called this! If you change this, change the name of the child model in the Player object
     }
 
-    // Update is called once per frame
+    //Now for when the player controls the character's movement.
     void Update()
     {
-        
+        moveDirection.x = Input.GetAxis("Horizontal") * speed;
+
+        //Now the directional left-right stuff
+        if (Input.GetAxis ("Horizontal") < 0)
+        {
+            Vector3 velocity = Vector3.zero;
+            //This next line sets the default rotation based on player's direction
+            Vector3 desiredAngles = new Vector3(model.localEulerAngles.x, 90, model.localEulerAngles.z);
+            model.localEulerAngles = (Vector3.SmoothDamp(model.localEulerAngles, desiredAngles, ref velocity, 1 * Time.deltaTime));
+        }
+
+        else if (Input.GetAxis("Horizontal") > 0)
+        {
+            Vector3 velocity = Vector3.zero;
+            //This next line sets the default rotation based on player's direction
+            Vector3 desiredAngles = new Vector3(model.localEulerAngles.x, 270, model.localEulerAngles.z);
+            model.localEulerAngles = (Vector3.SmoothDamp(model.localEulerAngles, desiredAngles, ref velocity, 1 * Time.deltaTime));
+        }
+
+        //Now for the jumping stuff
+
+        if (characterController.isGrounded)
+        {
+            currentJumps = 0; //If the character is on the ground, then reset the number of current jumps to zero
+        }
+        else
+        {
+            moveDirection.y -= gravity * Time.deltaTime; //If the character is NOT grounded, then move them along the Y axis according to gravity
+        }
+
+        //Now we execute the jump when the character hits the jump key
+        if (Input.GetButtonDown("Jump") && currentJumps < maxJumps) //If the current number of jumps is less than max when Jump is pressed
+        {
+            playerAudioSource.PlayOneShot(playerJumpingSound);
+            moveDirection.y = jumpSpeed;
+            currentJumps ++;
+        }
+
+        //If we are in the air, apply gravity to the jump. After all, what goes up must come down.
+        if (characterController.isGrounded == false)
+        {
+            moveDirection.y -= gravity * Time.deltaTime;
+        }
+
+        //And now we get the actual character controller moving!
+        characterController.Move(moveDirection * Time.deltaTime);
+
     }
 }
