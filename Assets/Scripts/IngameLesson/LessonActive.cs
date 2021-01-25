5f0c7f04894e93bd2cdff89554f21f1fbc218d35
giftheck <1804007@students.ucreative.ac.uk>
2021-01-25 13:31:57 +0000

    Preproduction files 2

---
+++ Assets/Scripts/IngameLesson/LessonActive.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class LessonActive : MonoBehaviour
+{
+
+    public GameObject educationalCanvas;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (Input.GetKeyDown("e"))
+        {
+            Time.timeScale = 0.0f;
+            educationalCanvas.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/IngameLesson/LessonDeactivate.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LessonDeactivate : MonoBehaviour
+{
+    public GameObject educationalCanvas;
+
+    public void OnClick()
+    {
+        Time.timeScale = 1.0f;
+        educationalCanvas.SetActive(false);
+    }
+}
